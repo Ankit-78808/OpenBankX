f3f98290ea53f0349e62936e9b7ce4885c1aeda2
harini-u-s <Harini.US@cognizant.com>
2026-03-09 14:50:15 +0530

    first commit

---
+++ src/test/java/com/cts/openbankx/OpenbankxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.openbankx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenbankxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
