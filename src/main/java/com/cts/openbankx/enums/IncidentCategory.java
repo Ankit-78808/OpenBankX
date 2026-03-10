ec63093c53cbfdef43cba672b6ce92ff505a32dc
Kumar <2478675@cognizant.com>
2026-03-10 13:23:27 +0530

    table creation

---
+++ src/main/java/com/cts/openbankx/enums/IncidentCategory.java
@@ -0,0 +1,8 @@
+package com.cts.openbankx.enums;
+
+public enum IncidentCategory {
+	OUTAGE,
+    LATENCY,
+    SECURITY
+
+}
---
+++ src/main/java/com/cts/openbankx/model/APIUsageReport.java
@@ -0,0 +1,24 @@
+package com.cts.openbankx.model;
+
+import com.cts.openbankx.enums.ReportScopeKey;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+//@Entity
+public class APIUsageReport {
+	
+//	  @Column(name = "ReportID")
+//	   private Long ReportID;
+//
+//	    @Column(name = "Scope")
+//	    @Enumerated(EnumType.STRING)
+//	    private ReportScopeKey Scope;
+//
+//	   
+//	    @Column(name = "Metrics", columnDefinition = "TEXT")
+//	    private String Metrics;
+
+}
