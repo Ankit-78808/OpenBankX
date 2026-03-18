0b6e361080800e2de9cb40eaa67a148d4e208c7b
harini-u-s <Harini.US@cognizant.com>
2026-03-18 11:07:17 +0530

    Delete src/main/java/com/cts/openbankx/repository/IncidentRepository.java

--- src/main/java/com/cts/openbankx/repository/IncidentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cts.openbankx.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cts.openbankx.model.Incident;
-
-public interface IncidentRepository extends JpaRepository<Incident, Long> {
-
-}
