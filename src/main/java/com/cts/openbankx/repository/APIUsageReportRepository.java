8d3f3bd7d7c9598d279b81a40496235a75d89bc5
harini-u-s <Harini.US@cognizant.com>
2026-03-18 11:06:27 +0530

    Delete src/main/java/com/cts/openbankx/repository/APIUsageReportRepository.java

--- src/main/java/com/cts/openbankx/repository/APIUsageReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cts.openbankx.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cts.openbankx.model.APIUsageReport;
-
-public interface APIUsageReportRepository extends JpaRepository<APIUsageReport, Long> {
-
-}
