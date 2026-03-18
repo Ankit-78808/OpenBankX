d28ac61f32a9f0345b402b501f2f544b2454cba0
harini-u-s <Harini.US@cognizant.com>
2026-03-18 11:07:28 +0530

    Delete src/main/java/com/cts/openbankx/repository/RateLimitRepo.java

--- src/main/java/com/cts/openbankx/repository/RateLimitRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cts.openbankx.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cts.openbankx.model.RateLimit;
-
-public interface RateLimitRepo extends JpaRepository<RateLimit, Long> {
-
-}
