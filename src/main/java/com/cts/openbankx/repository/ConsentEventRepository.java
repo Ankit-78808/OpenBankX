264d626b12d82d6e2b253015832b1f88e352636b
harini-u-s <Harini.US@cognizant.com>
2026-03-18 11:06:53 +0530

    Delete src/main/java/com/cts/openbankx/repository/ConsentEventRepository.java

--- src/main/java/com/cts/openbankx/repository/ConsentEventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cts.openbankx.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cts.openbankx.model.ConsentEvent;
-
-public interface ConsentEventRepository extends JpaRepository<ConsentEvent, Long> {
-
-}
