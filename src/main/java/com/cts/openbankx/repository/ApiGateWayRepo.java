740a542a1ae0e1eb8a3ecbf0627f9f753e70e50f
harini-u-s <Harini.US@cognizant.com>
2026-03-18 11:06:40 +0530

    Delete src/main/java/com/cts/openbankx/repository/ApiGateWayRepo.java

--- src/main/java/com/cts/openbankx/repository/ApiGateWayRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cts.openbankx.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cts.openbankx.model.ApiGateWay;
-
-public interface ApiGateWayRepo extends JpaRepository<ApiGateWay, Long> {
-
-}
