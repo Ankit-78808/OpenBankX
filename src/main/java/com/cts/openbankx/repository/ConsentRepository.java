214cb303fa6e3879ff7061edb828c5ad7828fa6f
harini-u-s <Harini.US@cognizant.com>
2026-03-18 11:07:05 +0530

    Delete src/main/java/com/cts/openbankx/repository/ConsentRepository.java

--- src/main/java/com/cts/openbankx/repository/ConsentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cts.openbankx.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cts.openbankx.model.Consent;
-
-public interface ConsentRepository extends JpaRepository<Consent, Long>{
-
-}
